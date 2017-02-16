42b65c9b827f960c67f5cdba31696cbe9b4c5a56
ychoy <yanyinchoy@gmail.com>
2017-02-16 11:59:57 -0800

    heroku deploy add changes #3

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
 
-app.user(bodyParser.urlencoded({extended: true}));
+//app.user(bodyParser.urlencoded({extended: true}));
 
 app.listen(process.env.PORT || 3000);
