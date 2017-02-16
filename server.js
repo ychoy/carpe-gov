cd8efec0226f8e66e2809b083df6e49ef163cf9a
ychoy <yanyinchoy@gmail.com>
2017-02-16 12:17:31 -0800

    fixed 404 error heroku deploy

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ var express = require('express'),
 
 //app.user(bodyParser.urlencoded({extended: true}));
 
+// define a root route: localhost:3000/
+app.get('/', function (req, res) {
+  res.sendFile('/views/index.html' , { root : __dirname});
+});
+
 app.listen(process.env.PORT || 3000);
