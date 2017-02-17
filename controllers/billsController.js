1192d960cefa8e54f5502731b5b2ec4b6de7ca29
westondombroski <westondombroski@gmail.com>
2017-02-16 19:46:49 -0800

    working on update

--- controllers/billsController.js
+++ controllers/billsController.js
@@ -8,13 +8,30 @@ function index(req, res) {
   });
 }
 
+function update(req, res) {
+  // find one bill by id, update it based on request body,
+  // and send it back as JSON
+  console.log('updating with data', req.body);
+
+  db.bill.findById(req.params.billId, function(err, foundBill) {
+    if(err) { console.log('billsController.update error', err); }
+    foundBill.title = req.body.title;
+    foundBill.summary = req.body.summary;
+    foundBill.sponsor = req.body.sponsor;
+    foundBill.textUrl = req.body.textUrl;
+    foundBill.latestAction = req.body.latestAction;
+    foundBill.save(function(err, savedBill) {
+      if(err) { console.log('saving altered bill failed'); }
+      res.json(savedBill);
+    });
+  });
+}
+
 // export public methods here
 module.exports = {
   index: index,
   //create: create,
   //show: show,
   //destroy: destroy,
-  //update: update
+  update: update
 };
-
-
