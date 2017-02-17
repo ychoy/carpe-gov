be7de1408e5bcc79250b8c4c621455c90e12ab0e
westondombroski <westondombroski@gmail.com>
2017-02-17 11:51:11 -0800

    edit and save changes functionality now working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,6 +17,11 @@ $(document).ready(function() {
     });
     $(this).trigger("reset");
   });
+
+//catch and handle click on Edit and Delete Bill buttons
+$('#bills').on('click', '.edit-bill', handleBillEditClick);
+$('#bills').on('click', '.save-bill', handleSaveChangesClick);
+//$('#bills').on('click', '.delete-bill', handleDeleteBillClick);
 });
 
 
@@ -30,7 +35,7 @@ function renderBill(bill) {
   console.log('rendering bill', bill);
 
   var billHtml = (`
-    <div class="row album" data-album-id="${bill._id}">
+    <div class="row bill" data-bill-id="${bill._id}">
       <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
           <div class="panel-body">
@@ -51,11 +56,11 @@ function renderBill(bill) {
                     <span class='bill-sponsor'>${bill.sponsor}</span>
                   </li>
                   <li class="list-group-item">
-                    <h4 class='inline-header'>Text:<a href=${bill.textUrl}> Read Text</a></h4>
+                    <h4 class='inline-header bill-text-url'>Text:<a href=${bill.textUrl}>Read Text</a></h4>
                   </li>
                   <li class="list-group-item">
                     <h4 class='inline-header'>Latest Action:</h4>
-                    <span class='bill-latestAction'>${bill.latestAction}</span>
+                    <span class='bill-latest-action'>${bill.latestAction}</span>
                   </li>
                 </ul>
 
@@ -65,7 +70,7 @@ function renderBill(bill) {
             <div class='panel-footer col-md-10 col-md-offset-1'>
               <div class='panel-footer'>
                 <button class='btn btn-primary add-bill'>Add Bill</button>
-                <button type="submit" class="btn btn-info text-right">Edit</button>
+                <button type="submit" class="btn btn-info text-right edit-bill">Edit</button>
                 <button class='btn btn-danger delete-bill hidden'>Delete Bill</button>
                 <button class='btn btn-success save-bill hidden'>Save Changes</button>
               </div>
@@ -77,4 +82,75 @@ function renderBill(bill) {
     <!-- end one bill -->
   `);
   $('#bills').prepend(billHtml);
+}
+
+function handleBillEditClick(e){
+    var $billRow = $(this).closest('.bill');
+    var billId = $billRow.data('bill-id');
+    console.log('edit bill', billId);
+
+    // show the save changes button
+    $billRow.find('.save-bill').toggleClass('hidden');
+    // hide the edit button
+    $billRow.find('.edit-bill').toggleClass('hidden');
+
+    // get the bill title and replace its field with an input element
+    var billTitle = $billRow.find('span.bill-title').text();
+    $billRow.find('span.bill-title').html('<input class="edit-bill-title" value="' + billTitle + '"></input>');
+
+    // get the bill summary and replace its field with an input element
+    var billSummary = $billRow.find('span.bill-summary').text();
+    $billRow.find('span.bill-summary').html('<input class="edit-bill-summary" value="' + billSummary + '"></input>');
+
+    // get the bill sponsor and replace its field with an input element
+    var billSponsor = $billRow.find('span.bill-sponsor').text();
+    $billRow.find('span.bill-sponsor').html('<input class="edit-bill-sponsor" value="' + billSponsor + '"></input>');
+
+    // get the bill text url and replace its field with an input element
+    var billTextUrl = $billRow.find('span.bill-text-url').text();
+    $billRow.find('span.bill-text-url').html('<input class="edit-bill-text-url" value="' + billTextUrl + '"></input>');
+
+    // get the bill latest action and replace its field with an input element
+    var billLatestAction = $billRow.find('span.bill-latest-action').text();
+    $billRow.find('span.bill-latest-action').html('<input class="edit-bill-latest-action" value="' + billLatestAction + '"></input>');
+}
+
+
+
+// after editing an album, when the save changes button is clicked
+function handleSaveChangesClick(e) {
+  var billId = $(this).closest('.bill').data('bill-id'); // $(this).closest would have worked fine too
+  var $billRow = $('[data-bill-id=' + billId + ']');
+
+  var data = {
+    title: $billRow.find('.edit-bill-title').val(),
+    summary: $billRow.find('.edit-bill-summary').val(),
+    sponsor: $billRow.find('.edit-bill-sponsor').val(),
+    textUrl: $billRow.find('.edit-bill-text-url').val(),
+    latestAction: $billRow.find('.edit-bill-latest-action').val()
+  };
+  console.log('PUTing data for bill', billId, 'with data', data);
+
+  $.ajax({
+    method: 'PUT',
+    url: '/api/bills/' + billId,
+    data: data,
+    success: handleBillUpdatedResponse
+  });
+}
+
+
+
+//onsuccess function of put ajax call PUTing data for bill
+function handleBillUpdatedResponse(data) {
+  console.log('response to update', data);
+
+  var billId = data._id;
+  // scratch this bill from the page
+  $('[data-bill-id=' + billId + ']').remove();
+  // and then re-draw it with the updates
+  renderBill(data);
+
+  // BONUS: scroll the change into view
+  //$('[data-bill-id=' + billId + ']')[0].scrollIntoView();
 }
