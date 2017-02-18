c0832268e933e0c9920164859f787dcb723da492
westondombroski <westondombroski@gmail.com>
2017-02-18 11:18:32 -0800

    cleaned up the code a bit, added comments, still working on getting the cancel edit button to work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,20 +6,21 @@ $(document).ready(function() {
     success: renderMultipleBills
   });
 
+//POSTs new bill from add bill dropdown form
   $('#bill-form form').on('submit', function(e) {
     e.preventDefault();
     var formData = $(this).serialize();
-  //var $modal =
-    console.log('formData', formData);
     $.ajax({
       method: 'POST',
       url: '/api/bills',
       data: formData,
       success: renderBill  //render the server's response
     });
+    $('[data-bill-id=' + billId + ']')[0].scrollIntoView();
     $(this).trigger("reset");
   });
 
+  $('#bills').on('click', '.cancel-edit', handleCancelEditClick);
   //$modal.modal('hide');
 
   //catch and handle click on Edit, Save, Delete and Cancel-Edit Bill buttons
@@ -28,19 +29,22 @@ $(document).ready(function() {
   $('#bills').on('click', '.delete-bill', handleDeleteBillClick);
   $('#bills').on('click', '.cancel-edit', handleCancelEditClick);
 
+  //click add bill button to get dropdown add bill form
   $(function(){
     $('legend').click(function(){
-      $(this).nextAll('div').toggle('hidden');
+      $(this).nextAll('div').toggleClass("hidden");
     });
   });
 });
 
+// initial onsuccess function to GET all bills and render them to page
 function renderMultipleBills(bills) {
   bills.forEach(function(bill) {
     renderBill(bill);
   });
 }
 
+// onsuccess function of POST which renders add bill form input to page
 function renderBill(bill) {
   var billHtml = (`
     <div class='row bill' data-bill-id='${bill._id}'>
@@ -65,8 +69,8 @@ function renderBill(bill) {
                   </li>
                   <li class='list-group-item'>
                     <h4 class='inline-header'>Text:</h4>
-                    <span class='bill-text-url' href="${bill.textUrl}">Read Text
-                    </span>
+                    <a class='bill-text-url' href="${bill.textUrl}">Read Text
+                    </a>
                   </li>
                   <li class='list-group-item'>
                     <h4 class='inline-header'>Latest Action:</h4>
@@ -78,11 +82,11 @@ function renderBill(bill) {
                 <div><button type='submit' class='btn btn-info text-right
                 edit-bill'>Edit</button></div> <br/>
                 <div><button type='submit' class='btn btn-success save-bill
-                hidden'>Save Changes</button></div> <br/>
+                hidden'>Save</button></div> <br/>
                 <div><button type='submit' class='btn btn-danger delete-bill
-                hidden'>Delete Bill</button></div> <br/>
-                <div><button type='button' data-dismiss='modal' class='btn
-                btn-default cancel-edit hidden'>Cancel</button></div> <br/>
+                hidden'>Delete</button></div> <br/>
+                <div><button type='button' class='btn btn-default cancel-edit
+                hidden'>Cancel</button></div> <br/>
               </div>
             </div>
             <!-- end of billinternal row -->
@@ -99,36 +103,30 @@ function renderBill(bill) {
   $('#bills').append(billHtml);
 }
 
+//handles the click on the Edit button of each bill, renders edit bill form
 function handleBillEditClick(e){
     var $billRow = $(this).closest('.bill');
-    var billId = $billRow.data('bill-id');
-
     // show the save changes/delete buttons
     $billRow.find('.save-bill').toggleClass('hidden');
     $billRow.find('.delete-bill').toggleClass('hidden');
     $billRow.find('.cancel-edit').toggleClass('hidden');
     // hide the edit button
     $billRow.find('.edit-bill').toggleClass('hidden');
-
     // get the bill title and replace its field with an input element
     var billTitle = $billRow.find('span.bill-title').text();
-    $billRow.find('span.bill-title').html('<input class="edit-bill-title" value="' + billTitle + '"></input>');
-
+    $billRow.find('span.bill-title').html('<input class="edit-bill-title form-control" value="' + billTitle + '"></input>');
     // get the bill summary and replace its field with an input element
     var billSummary = $billRow.find('span.bill-summary').text();
-    $billRow.find('span.bill-summary').html('<input class="edit-bill-summary" value="' + billSummary + '"></input>');
-
+    $billRow.find('span.bill-summary').html('<input class="edit-bill-summary form-control" rows="7" value="' + billSummary + '"></input>');
     // get the bill sponsor and replace its field with an input element
     var billSponsor = $billRow.find('span.bill-sponsor').text();
-    $billRow.find('span.bill-sponsor').html('<input class="edit-bill-sponsor" value="' + billSponsor + '"></input>');
-
+    $billRow.find('span.bill-sponsor').html('<input class="edit-bill-sponsor form-control" value="' + billSponsor + '"></input>');
     // get the bill text url and replace its field with an input element
-    var billTextUrl = $billRow.find('span.bill-text-url').attr('href');
-    $billRow.find('span.bill-text-url').html('<input class="edit-bill-text-url" value="' + billTextUrl + '"></input>');
-
+    var billTextUrl = $billRow.find('.bill-text-url').attr('href');
+    $billRow.find('.bill-text-url').html('<input class="edit-bill-text-url form-control" value="' + billTextUrl + '"></input>');
     // get the bill latest action and replace its field with an input element
     var billLatestAction = $billRow.find('span.bill-latest-action').text();
-    $billRow.find('span.bill-latest-action').html('<input class="edit-bill-latest-action" value="' + billLatestAction + '"></input>');
+    $billRow.find('span.bill-latest-action').html('<input class="edit-bill-latest-action form-control" value="' + billLatestAction + '"></input>');
 }
 
 // after editing an album, when the save changes button is clicked
@@ -142,7 +140,6 @@ function handleSaveChangesClick(e) {
     textUrl: $billRow.find('.edit-bill-text-url').val(),
     latestAction: $billRow.find('.edit-bill-latest-action').val()
   };
-
   $.ajax({
     method: 'PUT',
     url: '/api/bills/' + billId,
@@ -162,7 +159,6 @@ function handleBillUpdatedResponse(data) {
 // when a delete button for an album is clicked
 function handleDeleteBillClick(e) {
   var billId = $(this).parents('.bill').data('bill-id');
-  console.log('someone wants to delete bill id=' + billId );
   $.ajax({
     url: '/api/bills/' + billId,
     method: 'DELETE',
@@ -173,11 +169,10 @@ function handleDeleteBillClick(e) {
 // callback after DELETE /api/albums/:id
 function handleDeleteBillSuccess(data) {
   var deletedBillId = data._id;
-  console.log('removing the following bill from the page:', deletedBillId);
   $('div[data-bill-id=' + deletedBillId + ']').remove();
 }
 
-// cancelling the edit bills button and returning bill to default
+// cancels edits in edit bills form and returns to homepage
 function handleCancelEditClick(e) {
 
 }
